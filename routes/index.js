0a7514db50782a92a903672a633aaff923edfe1c
Shalom <oyeniranexcellenced@gmail.com>
2019-03-07 16:08:38 +0100

    Added routes to different functions in userQueries file

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const userQueries = require('../controllers/userQueries')
+const userQueries = require('../controllers/userQueries.js')
+
+router.post('/signup', userQueries.createUser);
+
+router.post('/updateProfile', userQueries.UpdateUser);
+
+router.post('/deleteUser', userQueries.DeleteUser);
+
+router.post('/getCurrentUserData', userQueries.CurrentUserData)
+
+module.exports = router; 
