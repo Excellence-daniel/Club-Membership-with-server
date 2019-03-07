033547679d47843dbd30eccf8a495434da3b4130
Shalom <oyeniranexcellenced@gmail.com>
2019-03-07 16:30:51 +0100

    Modified the function that fetched current user data

--- controllers/userQueries.js
+++ controllers/userQueries.js
@@ -83,25 +83,25 @@ exports.CurrentUserData = async function (req, res) {
     const data = req.body;
     const userEmail = data.userEmail; 
     console.log("User Email", userEmail);  
-        try {
-            database.collection('Users').where("Email", "==", userEmail).get()   //get from collection USERS using email
-            .then((snapshot)=>{
-                snapshot.forEach((doc)=>{
-                    console.log("data",doc, doc.data())
-                    userData = doc.data()
-                    userID = doc.id
-                })
-
-                if (userData){
-                    res.send({UserEmail : userEmail, userData : userData, userID : userID})  //if data is gotten
-                }else{
-                    res.send({UserEmail : userEmail, userData : null, userID : null, statusmessage : "no matching documents in firebase"})     //data is not gotten 
-                } 
+    try {
+        database.collection('Users').where("Email", "==", userEmail).get()   //get from collection USERS using email
+        .then((snapshot)=>{
+            snapshot.forEach((doc)=>{
+                console.log("data",doc, doc.data())
+                userData = doc.data()
+                userID = doc.id
             })
-            console.log("Current User Email", userEmail, "---/getCurrentUserData")
-        }
-        catch(err){
-            console.log(err.message)
-            res.send({UserEmail : null})
-        }
-    } 
+
+            if (userData){
+                res.send({UserEmail : userEmail, userData : userData, userID : userID})  //if data is gotten
+            }else{
+                res.send({UserEmail : userEmail, userData : null, userID : null, statusmessage : "no matching documents in firebase"})     //data is not gotten 
+            } 
+        })
+        console.log("Current User Email", userEmail, "---/getCurrentUserData")
+    }
+    catch(err){
+        console.log(err.message)
+        res.send({UserEmail : null})
+    }
+} 
