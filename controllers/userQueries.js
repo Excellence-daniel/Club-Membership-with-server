8cc994955e122ddca674acd2628342985c2d4e6e
Shalom <oyeniranexcellenced@gmail.com>
2019-03-09 15:34:55 +0100

    Added semi colons to end lines

--- controllers/userQueries.js
+++ controllers/userQueries.js
@@ -1,33 +1,33 @@
 var uuidv4 = require('uuid/v4'); //for generating unique IDs for users
 var validator = require('validator');
-var admin = require("firebase-admin");
+var admin = require('firebase-admin');
 var database = admin.firestore();
 
 exports.UpdateUser = async function (req, res) {
-    const userData = req.body
+    const userData = req.body;
     try{
         await database.collection('Users').doc(userData.userID).update({
             Name : userData.Name, 
             Email : userData.Email,
             Address : userData.Address,
             PhoneNumber : userData.Phone
-        })
-        res.send({status : 200, statusmessage : "Success"})
+        });
+        res.send({status : 200, statusmessage : 'Success'});
     }
     catch(err){
-        res.send({status : 400, statusmessage : err.message , errorMessage : "Bad Request"})
+        res.send({status : 400, statusmessage : err.message , errorMessage : 'Bad Request'});
     }
 }
 
 
 exports.DeleteUser = async function (req, res) {
-    const userData = req.body
+    const userData = req.body;
     try { 
-        await database.collection('Users').doc(userData.userID).delete()
-        res.send({status : 200, statusmessage : "Profile Deleted"})
+        await database.collection('Users').doc(userData.userID).delete();
+        res.send({status : 200, statusmessage : 'Profile Deleted'});
     }
     catch(err){
-        res.send({status : 200, statusmessage : err.message})
+        res.send({status : 200, statusmessage : err.message});
     }
 }
 
@@ -36,26 +36,26 @@ exports.CurrentUserData = async function (req, res) {
     let userID; 
     const data = req.body;
     const userEmail = data.userEmail; 
-    console.log("User Email", userEmail);  
+    console.log('User Email', userEmail);  
     try {
-        database.collection('Users').where("Email", "==", userEmail).get()   //get from collection USERS using email
-        .then((snapshot)=>{
-            snapshot.forEach((doc)=>{
-                console.log("data",doc, doc.data())
-                userData = doc.data()
-                userID = doc.id
+        database.collection('Users').where('Email', '==', userEmail).get()   //get from collection USERS using email
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                console.log('data', doc, doc.data());
+                userData = doc.data();
+                userID = doc.id;
             })
 
-            if (userData){
-                res.send({status : 200, UserEmail : userEmail, userData : userData, userID : userID})  //if data is gotten
-            }else{
-                res.send({status : 400, UserEmail : userEmail, userData : null, userID : null, statusmessage : "no matching documents in firebase"})     //data is not gotten 
+            if (userData) {
+                res.send({status : 200, UserEmail : userEmail, userData : userData, userID : userID});  //if data is gotten
+            } else {
+                res.send({status : 400, UserEmail : userEmail, userData : null, userID : null, statusmessage : 'no matching documents in firebase'});     //data is not gotten 
             } 
         })
-        console.log("Current User Email", userEmail, "---/getCurrentUserData")
+        console.log('Current User Email', userEmail, '---/getCurrentUserData');
     }
     catch(err){
-        console.log(err.message)
-        res.send({status : 400, UserEmail : null})
+        console.log(err.message);
+        res.send({status : 400, UserEmail : null});
     }
 } 
