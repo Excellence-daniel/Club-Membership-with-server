4dc8b3e8bc8980706159a2253e6eb3edc70ad926
Shalom <oyeniranexcellenced@gmail.com>
2019-03-09 12:45:05 +0100

    Added a function that gets the current user details and returns important data to the front end

--- controllers/userQueries.js
+++ controllers/userQueries.js
@@ -3,52 +3,6 @@ var validator = require('validator');
 var admin = require("firebase-admin");
 var database = admin.firestore();
 
-exports.createUser = async function(req, res){
-    const data = req.body
-    var numbers = /\d+/;
-    var specialchars = /[ !@#$%^&*()`_+\-=\[\]{};':"\\|,.<>\/?]/; 
-    const isEmail = validator.isEmail(data.email)
-    const UserUUID = uuidv4();
-    console.log(UserUUID);
-    //check if the email data sent is an actual email
-    if (data.password.length > 9 && numbers.test(data.password) === true && specialchars.test(data.password) === true){
-        if (isEmail === true){
-            //check if a user with the email exists
-            var checkIfUserExists = await database.collection('Users').where("Email", "==", data.email).get()
-            console.log(checkIfUserExists.empty)
-            if (checkIfUserExists.empty === true){
-                admin.auth().createUser({
-                    email : data.email, 
-                    password : data.password
-                })
-                .then(()=>{
-                    database.collection('Users').doc().set({
-                        Name : data.name, 
-                        Email : data.email, 
-                        EmailVerified : false, 
-                        PhoneNumber : data.phone, 
-                        Address : data.address, 
-                        Password : data.password, 
-                        ClubsJoined : [], 
-                        UserID : UserUUID
-                    })
-                    console.log("User sign in successful"); 
-                    res.send({signInStatus : 'success'});
-                })
-                .catch(err=>{
-                    console.log("/signup ---", err.message)
-                })
-            } else {
-                res.send({status : 401, statusmessage : "A User with this Email already exists. Please use another email to register."})
-            }
-        } else {
-            res.send({status : 400, statusmessage : "Email not in the right format"})
-        }
-    } else {
-        res.send({status : 400 , statusmessage : "Password is not strong.!"})
-    }
-}
-
 exports.UpdateUser = async function (req, res) {
     const userData = req.body
     try{
