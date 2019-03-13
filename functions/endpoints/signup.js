d1a9f8d0a8f11ec6c04d687b78f14219bc9b679e
Shalom <oyeniranexcellenced@gmail.com>
2019-03-13 16:40:24 +0100

    File that holds the function for a user to sign up

--- functions/endpoints/signup.js
+++ functions/endpoints/signup.js
@@ -6,64 +6,64 @@ const database = admin.firestore();
 exports.signUp = function (req, res) {
     const data = req.body;
     const numbers = /\d+/;
-    const specialchars = /[ !@#$%^&*()`_+\-=[\]{};':'\\|,.<>/?]/; 
-    const isEmail = validator.isEmail(data.email); 
+    const specialchars = /[ !@#$%^&*()`_+\-=[\]{};':'\\|,.<>/?]/;
+    const isEmail = validator.isEmail(data.email);
     const UserUUID = uuidv4();
     let checkIfUserExists;
     console.log(UserUUID);
     //check if the email data sent is an actual email
-    if (data.password.length > 9 && numbers.test(data.password) && specialchars.test(data.password)){
+    if (data.password.length > 9 && numbers.test(data.password) && specialchars.test(data.password)) {
         if (isEmail) {      //validate email
             database.collection('Users').where('Email', '==', data.email).get()
-            .then((doc) => {
-                checkIfUserExists = doc.empty; //check if any profile is created with the email
-                if (checkIfUserExists) {
-                    try {
-                        admin.auth().createUser({
-                            email: data.email,
-                            password: data.password
-                        })  //create user auth
-                        .then(() => {
-                            database.collection('Users').doc().set({
-                                Name: data.name,
-                                Email: data.email,
-                                EmailVerified: false,
-                                PhoneNumber: data.phone,
-                                Address: data.address,
-                                Password: data.password,
-                                ClubsJoined: [],
-                                UserID: UserUUID
-                            }) //create user profile
-                            .then(() => {
-                                console.log('User sign in successful');
-                                res.send({ status : 200, signInStatus: 'success' });
-                            })
-                            .catch((err)=>{
-                                console.log(err);
-                                res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request I' });
-                            })
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                            res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request II' });
-                        })
+                .then((doc) => {
+                    checkIfUserExists = doc.empty; //check if any profile is created with the email
+                    if (checkIfUserExists) {
+                        try {
+                            admin.auth().createUser({
+                                email: data.email,
+                                password: data.password
+                            })  //create user auth
+                                .then(() => {
+                                    database.collection('Users').doc().set({
+                                        Name: data.name,
+                                        Email: data.email,
+                                        EmailVerified: false,
+                                        PhoneNumber: data.phone,
+                                        Address: data.address,
+                                        Password: data.password,
+                                        ClubsJoined: [],
+                                        UserID: UserUUID
+                                    }) //create user profile
+                                        .then(() => {
+                                            console.log('User sign in successful');
+                                            res.send({ status: 200, signInStatus: 'success' });
+                                        })
+                                        .catch((err) => {
+                                            console.log(err);
+                                            res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request I' });
+                                        })
+                                })
+                                .catch((err) => {
+                                    console.log(err);
+                                    res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request II' });
+                                })
+                        }
+                        catch (err) {
+                            console.log(err, err.message);
+                            res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request III' });
+                        }
+                    } else {
+                        res.send({ status: 401, statusmessage: 'A User with this Email already exists. Please use another email to register.' });
                     }
-                    catch (err) {
-                        console.log(err, err.message);
-                        res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request III' });
-                    }
-                } else {
-                    res.send({status : 401, statusmessage : 'A User with this Email already exists. Please use another email to register.'});
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-                res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request IV' });
-            })
+                })
+                .catch((err) => {
+                    console.log(err)
+                    res.send({ status: err.code, statusmessage: err.message, errorMessage: 'Bad Request IV' });
+                })
         } else {
-            res.send({status : 400, statusmessage : 'Email not in the right format'});
+            res.send({ status: 400, statusmessage: 'Email not in the right format' });
         }
     } else {
-        res.send({status : 400 , statusmessage : 'Password is not strong.!'});
+        res.send({ status: 400, statusmessage: 'Password is not strong.!' });
     }
-}
+};
