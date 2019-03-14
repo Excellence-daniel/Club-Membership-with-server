c3bbf449b89a898b3b39303a4f2214e424aa4921
Shalom <oyeniranexcellenced@gmail.com>
2019-03-14 16:28:24 +0100

    Created a file holds a function to allow new users to verify their Email

---
+++ functions/src/verifyEmail/index.ts
@@ -0,0 +1,31 @@
+// import * as uuidv4 from 'uuid/v4';
+// import * as validator from 'validator';
+import * as admin from 'firebase-admin';
+const database = admin.firestore();
+
+export const verifyEmail = async function(req, res) {
+    const userInfo = req.body; 
+    let userData;
+    let userID;
+    try {
+        const getUserByID = await database.collection('Users').where('UserID', '==', userInfo.UserToken).get();
+        getUserByID.forEach((doc)=>{
+            userData = doc.data(); 
+            userID = doc.id; 
+        })
+
+        if (userData.EmailVerified !== true){
+            await database.collection('Users').doc(userID).update({
+                EmailVerified : true
+            })
+            res.send({status : 200, statusmessage : 'success'});
+            console.log('Email Verified Successfully');
+        } else {
+            res.send({status : 401 , statusmessage : 'Email Verfied Already'});
+        }
+    }
+    catch(err){
+        console.log(err.message);
+        res.send({status : 400, statusmessage : err.message, errorMessage : 'Bad Request'});
+    }
+}
