d69a8d6491c69d77a0f34e1411cbf9e1c563ef29
Shalom <oyeniranexcellenced@gmail.com>
2019-03-14 08:07:47 +0100

    Wrote a function to update and delete a user

--- functions/endpoints/userQueries.js
+++ functions/endpoints/userQueries.js
@@ -1,50 +1,75 @@
-// var uuidv4 = require('uuid/v4'); //for generating unique IDs for users
-// var validator = require('validator');
-// var admin = require('firebase-admin');
-// var database = admin.firestore();
+var uuidv4 = require('uuid/v4'); //for generating unique IDs for users
+var validator = require('validator');
+var admin = require('firebase-admin');
+var database = admin.firestore();
 
-// exports.signUp = async(request, response) => {
-//     const data = req.body;
-//     const numbers = /\d+/;
-//     const specialchars = /[ !@#$%^&*()`_+\-=[\]{};':'\\|,.<>/?]/; 
-//     const isEmail = validator.isEmail(data.email)
-//     const UserUUID = uuidv4();
-//     console.log(UserUUID);
-//     //check if the email data sent is an actual email
-//     if (data.password.length > 9 && numbers.test(data.password) && specialchars.test(data.password)){
-//         if (isEmail) {
-//             //check if a user with the email exists
-//             var checkIfUserExists = await database.collection('Users').where('Email', '==', data.email).get();
-//             if (checkIfUserExists.empty) {
-//                 try { 
-//                     await admin.auth().createUser({
-//                         email: data.email,
-//                         password: data.password
-//                     })
-//                     await database.collection('Users').doc().set({
-//                         Name: data.name,
-//                         Email: data.email,
-//                         EmailVerified: false,
-//                         PhoneNumber: data.phone,
-//                         Address: data.address,
-//                         Password: data.password,
-//                         ClubsJoined: [],
-//                         UserID: UserUUID
-//                     })
-//                     console.log('User sign in successful');
-//                     response.send({ status : 200, signInStatus: 'success' });
-//                 } 
-//                 catch (err) {
-//                     console.log('/signup ---', err.message);       
-//                     response.send({status : 400, statusmessage : err.message, errorMessage : 'Bad Request'});            
-//                 }
-//             } else {
-//                 response.send({status : 401, statusmessage : 'A User with this Email already exists. Please use another email to register.'});
-//             }
-//         } else {
-//             response.send({status : 400, statusmessage : 'Email not in the right format'});
-//         }
-//     } else {
-//         response.send({status : 400 , statusmessage : 'Password is not strong.!'});
-//     }
-// }
+exports.getCurrentUserData = function (req, res) {
+    let userData; 
+    let userID; 
+    const data = req.body;
+    const userEmail = data.userEmail; 
+    console.log('User Email', userEmail);  
+    try {
+        database.collection('Users').where('Email', '==', userEmail).get()
+        .then((snapshot)=>{
+            snapshot.forEach((doc) => {
+                console.log('data', doc, doc.data());
+                userData = doc.data();
+                userID = doc.id;
+            })
+            if (userData) {
+                res.send({status : 200, UserEmail : userEmail, userData : userData, userID : userID});  //if data is gotten
+            } else {
+                res.send({status : 400, UserEmail : userEmail, userData : null, userID : null, statusmessage : 'no matching documents in firebase'}); //data is not gotten 
+            } 
+            console.log('Current User Email', userEmail, '---/getCurrentUserData');
+        })
+        .catch((err)=>{
+            console.log(err.message);
+            res.send({status : err.code, statusmessage : err.message})
+        })
+    }
+    catch(err){
+        console.log(err.message);
+        res.send({status : 400, UserEmail : null});
+    }
+}
+
+exports.UpdateUser = function (req, res) {
+    const userData = req.body;
+    try{
+        database.collection('Users').doc(userData.userID).update({
+            Name : userData.Name, 
+            Email : userData.Email,
+            Address : userData.Address,
+            PhoneNumber : userData.Phone
+        })
+        .then(()=>{
+            res.send({status : 200, statusmessage : 'Success'});
+        })
+        .catch((err)=>{
+            res.send({status : err.code, statusmessage : err.message});
+        }) 
+    } 
+    catch(err){
+        res.send({status : 400, statusmessage : err.message , errorMessage : 'Bad Request'});
+    }
+}
+
+
+exports.DeleteUser = function (req, res) {
+    const userData = req.body;
+    try { 
+        admin.auth().deleteUser(userData.uid)
+        .then(()=>{
+            res.send({status : 200, statusmessage : 'User Profile Deleted'});            
+        })
+        .catch((err)=>{
+            res.send({status : err.code, statusmessage : err.message})
+        })
+    }
+    catch(err){
+        res.send({status : 200, statusmessage : err.message, errorMessage : 'Error Deleting the user.'});
+    }
+}
+
