d9532e96a4752d43c33d4961e05574d3a8fab6a4
Shalom <oyeniranexcellenced@gmail.com>
2019-03-14 14:54:47 +0100

    Installed packages and imported them

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import * as validator from 'validator';
-import * as uuid from 'uuid';
+// import * as validator from 'validator';
+// import * as uuid from 'uuid';
 import * as cors from 'cors';
 import * as express from 'express';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const serviceAccount = require('../service_account.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://club-membership-87748.firebaseio.com'
+});
+
+// const database = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -11,3 +27,6 @@ import * as express from 'express';
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+
+// const validateEmail = validator.isEmail('ade@gmail.com');
